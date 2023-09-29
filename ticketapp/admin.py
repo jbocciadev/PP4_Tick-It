127fa2fb8d9f6778df5e77298423e55d82d90c18
jbocciadev <jboccia@gmail.com>
2023-09-29 14:10:04 +0000

    Add profile model to registration.

--- ticketapp/admin.py
+++ ticketapp/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
-from .models import Ticket, Team
+from .models import Ticket, Team, Profile
 
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'assigned_team')
     search_fields = ('id', 'author')
-    
+
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
-# admin.site.register(Ticket)
-# admin.site.register(Team)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'surname', 'email', 'role')
