c33f774dfe1434088a01ff6277f158ada97911ba
jbocciadev <jboccia@gmail.com>
2023-10-05 10:31:53 +0000

    Setup cloudinary.

--- tickit/settings.py
+++ tickit/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'ticketapp',
 ]
 
@@ -137,9 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
