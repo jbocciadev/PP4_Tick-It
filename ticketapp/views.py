2f7aed1818798b1cf8a4c15c5d6f1fd54602cb68
jbocciadev <jboccia@gmail.com>
2023-10-09 10:36:37 +0000

    Implement auto-redirect on authenticated user in landing page.

--- ticketapp/views.py
+++ ticketapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.views import generic, View
 from django.contrib.auth.models import User
 from .models import Ticket, Profile, Team
@@ -7,7 +7,10 @@ from .models import Ticket, Profile, Team
 # class TicketList(generic.ListView):
 
 def landing_page(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('/ticket_list')
+    else:
+        return render(request, 'index.html')
 
 
 class TicketList(generic.ListView):
